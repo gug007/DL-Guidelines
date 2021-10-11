5c341c485d0603b6362188b74895a2f93e9e7dfa
gug007 <gug007@i.ua>
2021-10-11 11:19:10 +0400

    add form

--- src/GuideForm.js
+++ src/GuideForm.js
@@ -7,43 +7,41 @@ export default function GuideForm({ onSave, onClose }) {
   };
 
   return (
-    <>
-      <Form
-        onSubmit={handleSave}
-        render={({ handleSubmit }) => (
-          <div className="create-new-guide pa-small pr-large">
-            <form onSubmit={handleSubmit}>
-              <div className="form-group input-field">
-                <div className="cf mb-small">
-                  <label htmlFor="new-guide-name">Guide Title</label>
-                  <button
-                    style={{ margin: 0 }}
-                    className="btn btn-primary right"
-                    type="submit"
-                  >
-                    <i className="fa fa-close" aria-hidden="true" /> Save
-                  </button>
-                  <a
-                    className="btn btn-default right mr-medium"
-                    title="Edit"
-                    role="button"
-                    tabIndex="0"
-                    onClick={onClose}
-                  >
-                    <i className="fa fa-eye" aria-hidden="true" /> Cancel
-                  </a>
-                </div>
-                <Field
-                  name="name"
-                  component="input"
-                  className="form-control input-default"
-                />
-                <input type="text" name="hidden-field" className="hidden" />
+    <Form
+      onSubmit={handleSave}
+      render={({ handleSubmit }) => (
+        <div className="create-new-guide pa-small pr-large">
+          <form onSubmit={handleSubmit}>
+            <div className="form-group input-field">
+              <div className="cf mb-small">
+                <label htmlFor="new-guide-name">Guide Title</label>
+                <button
+                  style={{ margin: 0 }}
+                  className="btn btn-primary right"
+                  type="submit"
+                >
+                  <i className="fa fa-close" aria-hidden="true" /> Save
+                </button>
+                <a
+                  className="btn btn-default right mr-medium"
+                  title="Edit"
+                  role="button"
+                  tabIndex="0"
+                  onClick={onClose}
+                >
+                  <i className="fa fa-eye" aria-hidden="true" /> Cancel
+                </a>
               </div>
-            </form>
-          </div>
-        )}
-      />
-    </>
+              <Field
+                name="name"
+                component="input"
+                className="form-control input-default"
+              />
+              <input type="text" name="hidden-field" className="hidden" />
+            </div>
+          </form>
+        </div>
+      )}
+    />
   );
 }
